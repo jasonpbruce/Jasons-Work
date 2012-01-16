70b401f4641ad7524242577e563f42937d4d2107
Jason Bruce <jasonpbruce@gmail.com>
2012-01-16 11:34:32 -0800

    Challenge 2 Fixed

--- Triangle/Triangle/main.c
+++ Triangle/Triangle/main.c
@@ -8,22 +8,43 @@
 
 #include <stdio.h>
 
+float remainingAngle(float a, float b);
+void pIt(float remainingAngle);
+
 // Challenge Two - Triangle
 
-float angleC(float remainingAngle)
+// You can ignore this warning for now.
+float remainingAngle(float a, float b)
 {
-    float lastAngle = -180;
-    return lastAngle;
+    float resultingAngle = 180 - (a + b);
+    return resultingAngle;
     
+}
+
+// A Function to "print it" easiliy
+void pIt(float remainingAngle) {
+    printf("The third angle is %.4f\n", remainingAngle);
 }
 
 int main (int argc, const char * argv[])
 {
 
-    float angleA = 30.0;
-    float angleB = 60.0;
-    float angleC = remainingAngle(angleA,angleB);
-    printf("The third angle is %f.2\n", angleC);
+//    float angleA = 30.0;
+//    float angleB = 60.0;
+    float angleC = remainingAngle(30.0, 60.0);
+    pIt(angleC);
+    
+    // pIt does this: printf("The third angle is %f.2\n", angleC);
+    
+    angleC = remainingAngle(10.0, 20.0);
+    pIt(angleC);
+    
+    angleC = remainingAngle(90.0, 90.0);
+    pIt(angleC);
+    
+
     
     return 0;
 }
+
+// ok
