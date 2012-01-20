2397e38356aebcb3031b553c43629d911701684d
Jason Bruce <jasonpbruce@gmail.com>
2012-01-19 16:47:40 -0800

    Loop Error

--- FizzBuzz/FizzBuzz/main.c
+++ FizzBuzz/FizzBuzz/main.c
@@ -12,29 +12,28 @@
 int main (int argc, const char * argv[])
 {
 
-    int i;
-    for (i = 1; i <= 100; i++) {
+    for (int i = 1; i <= 100; i++) {
         printf("%d\n", i);
-    }
+      
+    // If divisible by 3 and 5 print "FizzBuzz"
     
-    // If divisible by 3 print "Fizz"
-    
-    if (i % 3 == 0) {
-        printf("Fizz!\n");
-    }
-    
-    // If divisible by 5 print "Buzz"
-    
-    if (i % 5 == 0) {
-        printf("Buzz!\n");
-    }
-    
-    // If divisible by 3 or 5 print "FizzBuzz"
-    
-    if (i % 3 == 0, i % 5 == 0) {
+    if (i % 15 == 0) {
         printf("FizzBuzz!\n");
+    
+    else (i % 5 == 0) {
+        printf("Buzz!\n");
+        
+    else (i % 3 == 0) {
+        printf("Fizz!\n")
+        
+    else (i % 1 == 0) {
+        printf("%d\n")
+        
+    }
+    }    
+    }
+    }
     }
     
     return 0;
 }
-
