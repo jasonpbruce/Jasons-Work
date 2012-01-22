ce999d75f408db5ae9b6829b2543e0105420a315
Jason Bruce <jasonpbruce@gmail.com>
2012-01-22 12:50:57 -0800

    Chapter 12

---
+++ TimeAfterTime/TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Jason Bruce on 1/22/12.
+//  Copyright (c) 2012 Fuimus Four. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *now = [NSDate date];
+        NSLog(@"The new date lives at %@", now);
+        
+    }
+    return 0;
+}
+
