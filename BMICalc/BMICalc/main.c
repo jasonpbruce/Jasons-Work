1c7af2ee2ebdab040fe02ec023a95763ac8611f3
Jason Bruce <jasonpbruce@gmail.com>
2012-01-22 00:03:10 -0800

    Chapter 10 & 11

---
+++ BMICalc/BMICalc/main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  BMICalc
+//
+//  Created by Jason Bruce on 1/21/12.
+//  Copyright (c) 2012 Fuimus Four. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+// Here is the declaration of the type Person
+    typedef struct {
+        float heightInMeters;
+        int weightInKilos;
+    } Person;
+    
+    float bodyMassIndex(Person p)
+    {
+        return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+    }
+
+                   
+main(int argc, const char * argv[])
+{
+        
+    Person person;
+    person.weightInKilos = 96;
+    person.heightInMeters = 1.8;
+    float bmi = bodyMassIndex(person);
+    printf("person has a BMI of %.2f\n", bmi);
+    
+    return 0;
+                          
+}
