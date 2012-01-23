e2aac4eceed2d6a66986f692a4feaf7e5155c53f
Chris Bruce <chrisabruce@gmail.com>
2012-01-22 19:09:14 -0800

    Cleaned up code indentation

--- BMICalc/BMICalc/main.c
+++ BMICalc/BMICalc/main.c
@@ -10,23 +10,22 @@
 
 // Here is the declaration of the type Person
 
-    typedef struct {
-        float heightInMeters;
-        int weightInKilos;
-    } Person;
-        
-        float bodyMassIndex(Person p)
-    {
-        return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
-    }
-        int main(int argc, const char * argv[])
-    {
-        Person person;
-        person.weightInKilos = 96;
-        person.heightInMeters = 1.8;
-        float bmi = bodyMassIndex(person);
-            printf("person has a BMI of %.2f\n", bmi);
-           
+typedef struct {
+    float heightInMeters;
+    int weightInKilos;
+} Person;
+
+float bodyMassIndex(Person p) {
+    return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+}
+
+int main(int argc, const char * argv[]) {
+    Person person;
+    person.weightInKilos = 96;
+    person.heightInMeters = 1.8;
+    float bmi = bodyMassIndex(person);
+    printf("person has a BMI of %.2f\n", bmi);
+    
     return 0;
-                          
+    
 }
